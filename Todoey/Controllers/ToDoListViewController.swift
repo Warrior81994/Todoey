d00d26213bc37e45efed75cba449bdea4fb3dd2b
Warrior81994 <Limgeorge20072@gmail.com>
2022-12-12 14:33:29 +0500

    Add CoreData instead UserDefaults

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -6,112 +6,161 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
-    
     var itemArray = [Item]()
-    
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-        print(dataFilePath)
-        
-        loadItems()
-        
-    }
-    
-    // MARK: - Tableview Datasource Merhods
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
-        let item = itemArray[indexPath.row]
-        
-        cell.textLabel?.text = item.title
-        
-        cell.accessoryType = item.done ? .checkmark : .none
-        
-        return cell
-    }
-    
-    // MARK: - TableView Delegate Methods
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        saveItems()
-            
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    // MARK: - Add New Items
-    
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
-        var textField = UITextField()
-        
-        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            // what will happen once the user clicks the Add Item Button on out UIAlert
-            
-            let newItem = Item()
-            newItem.title = textField.text!
-            
-            self.itemArray.append(newItem)
-            
-            self.saveItems()
-            
-        }
-        
-        alert.addTextField { alertTextField in
-            alertTextField.placeholder = "Create new item"
-            textField = alertTextField
-        }
-        
-        alert.addAction(action)
-        
-        present(alert, animated: true)
-        
-    }
-    
-    // MARK: - Model Manupulation Methods
-    
-    func saveItems() {
-        
-        let encoder = PropertyListEncoder()
-        
-        do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
-        } catch {
-            print("Error encoding item array, \(error)")
-        }
-        
-        self.tableView.reloadData()
-        
-    }
-    
-    func loadItems() {
-        
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding item array, \(error)")
+        var selectedCategory : Category? {
+            didSet{
+                loadItems()
             }
         }
         
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+
+            
+        }
+        
+        //MARK: - Tableview Datasource Methods
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return itemArray.count
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+            
+            let item = itemArray[indexPath.row]
+            
+            cell.textLabel?.text = item.title
+            
+            //Ternary operator ==>
+            // value = condition ? valueIfTrue : valueIfFalse
+            
+            cell.accessoryType = item.done ? .checkmark : .none
+            
+            return cell
+        }
+        
+        //MARK: - TableView Delegate Methods
+        
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
+
+    //        context.delete(itemArray[indexPath.row])
+    //        itemArray.remove(at: indexPath.row)
+            
+            itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+
+            saveItems()
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+        }
+        
+        //MARK: - Add New Items
+        
+        @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+            
+            var textField = UITextField()
+            
+            let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+                //what will happen once the user clicks the Add Item button on our UIAlert
+                
+                
+                let newItem = Item(context: self.context)
+                newItem.title = textField.text!
+                newItem.done = false
+                newItem.parentCategory = self.selectedCategory
+                self.itemArray.append(newItem)
+                
+                self.saveItems()
+            }
+            
+            alert.addTextField { (alertTextField) in
+                alertTextField.placeholder = "Create new item"
+                textField = alertTextField
+                
+            }
+            
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+        //MARK - Model Manupulation Methods
+        
+        func saveItems() {
+            
+            do {
+              try context.save()
+            } catch {
+               print("Error saving context \(error)")
+            }
+            
+            self.tableView.reloadData()
+        }
+        
+        func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+            
+            let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+            
+            if let addtionalPredicate = predicate {
+                request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, addtionalPredicate])
+            } else {
+                request.predicate = categoryPredicate
+            }
+
+            
+            do {
+                itemArray = try context.fetch(request)
+            } catch {
+                print("Error fetching data from context \(error)")
+            }
+            
+            tableView.reloadData()
+            
+        }
+       
+}
+
+
+// MARK: - Search bar Methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
     }
     
 }
-
